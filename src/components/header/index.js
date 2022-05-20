898d93d791bfb87921de99c20bcee1bb445f2647
alan soto <alansoto2015@outlook.com>
2022-05-19 23:47:19 -0300

    changed header padding

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import { Box, useColorMode, useMediaQuery } from '@chakra-ui/react';
+import { Box, useMediaQuery } from '@chakra-ui/react';
 import theme from '../../theme';
 import MenuDesktop from './desktop/MenuDesktop';
 import MenuMobile from './mobile/MenuMobile';
@@ -7,12 +7,13 @@ const Header = () => {
   const [isLargerThan900] = useMediaQuery('(min-width: 900px)');
   return (
     <Box
-      padding="2"
+      as={'header'}
       display="flex"
       alignItems="center"
       justifyContent="space-between"
       fontFamily={theme.fonts.coda}
       className="header"
+      padding={isLargerThan900 ? '10' : '2'}
     >
       {isLargerThan900 ? <MenuDesktop /> : <MenuMobile />}
     </Box>
