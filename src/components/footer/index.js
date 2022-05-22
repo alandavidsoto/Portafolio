5344b26b83d152c1acb309ad2cb5d81b868460bd
alan soto <alansoto2015@outlook.com>
2022-05-21 22:23:54 -0300

    add info to footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,29 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Link, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const Footer = () => {
-  return <Box h={'60px'} bg="blackAlpha.400"></Box>;
+  return (
+    <Box
+      h={'60px'}
+      bg="blackAlpha.400"
+      display={'flex'}
+      justifyContent="center"
+      alignItems={'center'}
+    >
+      <Text fontSize={'sm'}>
+        <Text as="span"></Text> Página web creada por{' '}
+        <Link
+          href="https://www.linkedin.com/in/alandavidsoto"
+          target={'_blank'}
+          style={{ textDecoration: 'none' }}
+          _hover={{ color: '#f3f' }}
+        >
+          Alan David Soto
+        </Link>
+        .
+      </Text>
+    </Box>
+  );
 };
 
 export default Footer;
